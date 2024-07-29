920aaf4af8503eaf406cf75fc757aa1f4464054a
damar <damar@gitlab.lab>
2024-07-29 18:57:36 +0700

    update

---
+++ components/PlayerCard.tsx
@@ -0,0 +1,98 @@
+'use client'
+import React from 'react';
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from './ui/card';
+
+import { Button } from './ui/button';
+import { Euro, MousePointer2, Percent } from 'lucide-react';
+import { Avatar, AvatarImage } from './ui/avatar';
+import { AvatarFallback } from '@radix-ui/react-avatar';
+
+interface PlayerCardProps {
+  // Add props here, e.g. playerName: string;
+  name?: string | any;
+  firstLastName?: string;
+  position?: string;
+  team?: string;
+  teamImg?:string;
+  image?: string | any;
+  tsb?: number | string;
+  nextMatches?: string[];
+  nextMatchesDiff: number[];
+  price?: number | string;
+  point?: number;
+
+}
+
+const PlayerCard: React.FC<PlayerCardProps> = (props) => {
+    const convertColorDifficulty = (code: number) => {
+        switch (code) {
+            case 1:
+                return 'bg-green-500 text-white'
+            case 2:
+                return 'bg-green-500 text-white'
+            case 3:
+                return 'bg-gray-200'
+            case 4:
+                return 'bg-red-500 text-white'
+            case 5:
+                return 'bg-red-900 text-white'
+            default:
+                return '';
+        }
+        return '';
+    }
+  return (
+    <Card className='w-11/12 md:w-1/4 lg:w-1/5 shadow-lg'>
+        {/* <CardHeader>
+            <CardTitle className='text-xl font-semibold'>{props.name}</CardTitle>
+            <CardDescription>{props.firstLastName}</CardDescription>
+        </CardHeader> */}
+        <CardContent className='pt-5'>
+            <div className="flex justify-between w-full items-center">
+                <div className="flex items-center">
+                    <Avatar>
+                        <AvatarImage src={props.image} alt={props.name} />
+                        {/* <AvatarFallback>CN</AvatarFallback> */}
+                    </Avatar>
+                    <Avatar>
+                        <AvatarImage src={props.teamImg} alt={props.team} />
+                        {/* <AvatarFallback>CN</AvatarFallback> */}
+                    </Avatar>
+                    <div className='p-2' >
+                    <p className='text-xl font-semibold'>{props.name}</p>
+                    <p className='text-sm text-gray-500'>{props.firstLastName}</p>
+                </div>
+                </div>
+                
+                
+
+                <div className="flex">
+                    <div className='h-10 w-10 p-3 rounded-full flex justify-center items-center font-semibold text-xs bg-gray-200'>
+                        {props.point?.toFixed(2) || 0}
+                    </div>
+                </div>
+            </div>
+            <div className='flex justify-start items-center w-full'>
+            <div className='w-full p-2 text-sm flex justify-center items-center'>{props.position}</div>
+                <div className='w-full p-2 text-sm flex justify-center items-center'>£{props.price}</div>
+                <div className='w-full p-2 text-sm flex justify-center items-center'><MousePointer2 className='p-1'/> {props.tsb}%</div>
+            </div>
+            <div className='flex items-center justify-between w-full'>
+                {props.nextMatches?.map((match: string, index) => (
+                    <div className={`w-full flex justify-center p-1 text-xs ${convertColorDifficulty(props.nextMatchesDiff[index])}`} key={index}>{match}</div>
+                ))}
+            </div>
+        </CardContent>
+        {/* <CardFooter className='flex justify-center'>
+            <div className='w-full'>
+                
+                <div className='mt-1'>
+                    <Button className='w-full'>Show Detail</Button>
+                </div>
+            </div>
+        </CardFooter> */}
+      </Card>
+  );
+};
+
+export default PlayerCard;
---
+++ services/expected_points.ts
@@ -0,0 +1,73 @@
+export default function expectedPoints(historyElements: any, element: any): number {
+    if (historyElements && historyElements.map((el: any) => el.code).includes(element.code)) {
+        const elData = historyElements.find((el:any) => el.code == element.code);
+
+        return xpFromxG(elData, element.element_type) + xpFromxA(elData, element.element_type) + xpFromCS(elData, element.element_type) + xpFromSaves(elData, element.element_type) + xpFromBonus(elData, element.element_type);
+    }
+    return 0;
+
+}
+
+function xpFromxG(elementDataHistory: any, position: number): number {
+    switch (position) {
+        case 1:
+            return elementDataHistory.expected_goals_per_90 * 10
+        case 2:
+            return elementDataHistory.expected_goals_per_90 * 6
+        case 3:
+            return elementDataHistory.expected_goals_per_90 * 5
+        case 4:
+            return elementDataHistory.expected_goals_per_90 * 4
+        default:
+            return 0;
+    }
+}
+
+function xpFromxA(elementDataHistory: any, position: number): number {
+    return elementDataHistory.expected_assists_per_90 * 3;
+}
+
+function xpFromCS(elementDataHistory: any, position: number): number {
+    switch (position) {
+        case 1:
+            return elementDataHistory.clean_sheets_per_90 * 4
+        case 2:
+            return elementDataHistory.clean_sheets_per_90 * 4
+        case 3:
+            return elementDataHistory.clean_sheets_per_90 * 1
+        case 4:
+            return 0
+        default:
+            return 0;
+    }
+}
+
+function xpFromSaves(elementDataHistory: any, position: number): number {
+    switch (position) {
+        case 1:
+            return Math.floor(elementDataHistory.saves_per_90 / 3)
+        case 2:
+            return 0
+        case 3:
+            return 0
+        case 4:
+            return 0
+        default:
+            return 0;
+    }
+}
+
+function xpFromBonus(elementDataHistory: any, position: number): number {
+    switch (position) {
+        case 1:
+            return 3
+        case 2:
+            return 3
+        case 3:
+            return 3
+        case 4:
+            return 3
+        default:
+            return 0;
+    }
+}
