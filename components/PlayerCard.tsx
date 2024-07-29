9f31ea2f823097542fd618dd0b11711a8da66d93
damar <damar@gitlab.lab>
2024-07-29 20:01:12 +0700

    fix

--- components/PlayerCard.tsx
+++ components/PlayerCard.tsx
@@ -42,7 +42,7 @@ const PlayerCard: React.FC<PlayerCardProps> = (props) => {
         return '';
     }
 
-    const convertColorPoint = (point: number): string => {
+    const convertColorPoint = (point: number | undefined): string => {
         if (point > 7) {
             return 'bg-green-500 text-white'
         } else if (point > 6) {
