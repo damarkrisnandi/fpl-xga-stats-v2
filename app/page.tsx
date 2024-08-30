99eca91ad00f74a026edfa0c9822275d439d6a55
damar <damar@gitlab.lab>
2024-08-30 14:09:27 +0700

    client bootstrap

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,14 @@
-// import AppFixtures from "@/components/main/AppFixtures";
-// import AppLeagueSummary from "@/components/main/AppLeagueSummary";
-// import AppTransferDeadline from "@/components/main/AppTransferDeadline";
-import AppElements from "@/components/main/AppElements";
-import { getBootstrap } from "@/services";
-import { leaguesData } from "@/utils";
+
+import AppElements from "@/components/main/AppElements";import MainPage from "@/components/main/MainPage";
+;
 import dynamic from "next/dynamic";
-import Image from "next/image";
 
-const AppTransferDeadline = dynamic(
-  () => import("@/components/main/AppTransferDeadline")
-);
-const AppLeagueSummary = dynamic(
-  () => import("@/components/main/AppLeagueSummary")
-);
-const AppFixtures = dynamic(() => import("@/components/main/AppFixtures"));
 
-export default async function Home() {
-  const bootstrap = await getBootstrap();
-  // const currentLeagues = leaguesData
-  //   .filter((league) => league.current)[0]
-  //   .children.filter((child: any) => child.league)
-  //   .map((l: any) => {
-  //     return { ...l, motwExist: l.name == "League B" ? false : true };
-  //   });
+export default function Home() {;
 
   return (
-    <main className="flex min-h-screen flex-col items-center pt-24">
-      <AppTransferDeadline bootstrap={bootstrap} />
-      <AppFixtures teams={bootstrap?.teams} events={bootstrap?.events} elements={bootstrap?.elements} element_stats={bootstrap?.element_stats}/>
-      <AppElements className="w-full"/>
+    <main className="min-h-screen pt-24">
+      <MainPage /> 
     </main> 
   );
 }
---
+++ components/main/MainPage.tsx
@@ -0,0 +1,48 @@
+"use client";
+import { useEffect, useState } from "react";
+import AppElements from "./AppElements";
+import AppFixtures from "./AppFixtures";
+import AppTransferDeadline from "./AppTransferDeadline";
+import { getBootstrapFromStorage } from "@/services";
+import AppSpinner from "./AppSpinner";
+import AppFailedToFetch from "./AppFailedToFetch";
+
+const MainPage = () => {
+  const [bootstrap, setBootstrap] = useState<any>(null);
+  useEffect(() => {
+    if (!bootstrap) {
+      getBootstrapFromStorage().then((value: any) => {
+        setBootstrap(value);
+      });
+    }
+  });
+  if (!bootstrap) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <AppSpinner />
+      </div>
+    );
+  }
+
+  if (bootstrap && bootstrap.error) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <AppFailedToFetch />
+      </div>
+    );
+  }
+  return (
+    <div className="flex flex-col items-center">
+      <AppTransferDeadline bootstrap={bootstrap} />
+      <AppFixtures
+        teams={bootstrap?.teams}
+        events={bootstrap?.events}
+        elements={bootstrap?.elements}
+        element_stats={bootstrap?.element_stats}
+      />
+      <AppElements className="w-full" />
+    </div>
+  );
+};
+
+export default MainPage;
