f1df834abd17e668656722cf1f0d68c8c28f0c22
Damar Krisnandi Ramdan <damarkrisnandi1202@gmail.com>
2024-09-06 18:06:07 +0700

    Update index.ts

--- utils/index.ts
+++ utils/index.ts
@@ -132,11 +132,11 @@ const calculateBaseExpected = (element: any, fixturesLen: number) => {
     assists,
 
   } = element;
-  const indexPer90 = (90 / minutes);
-  const xYC = (yellow_cards * (90 / minutes)) * -1;
-  const xRC = (red_cards * (90 / minutes)) * -2;
+  const indexPer90 = minutes > 0 ? (90 / minutes) : 0;
+  const xYC = (yellow_cards * indexPer90) * -1;
+  const xRC = (red_cards * indexPer90) * -2;
   const pMP = starts_per_90 >= 0.67 ? 2 : starts_per_90 == 0 ? 0 : 1;
-  const xOG = (own_goals * (90 / minutes)) * -1;
+  const xOG = (own_goals * indexPer90) * -1;
   const goalp90 = goals_scored * indexPer90;
   const assistp90 = assists * indexPer90; 
   if (element_type === 4) {
@@ -164,7 +164,7 @@ const calculateBaseExpected = (element: any, fixturesLen: number) => {
     const xPA = ((expected_assists_per_90 + assistp90) / 2) * 3;
     const xCS = starts_per_90 >= 0.67 ? (clean_sheets_per_90 >= 0.67 ? 4 : 0) : 0;
       const xGC = Math.floor(expected_goals_conceded_per_90 / 2) * -1;
-    const xSaves = Math.floor((saves * (90 / minutes)) / 3);
+    const xSaves = Math.floor((saves * indexPer90) / 3);
     xP =
       xPG +
       xPA +
