622f40e90aa1e0c9f8c43153f73d36e28387632a
damar <damar@gitlab.lab>
2024-10-29 11:25:55 +0700

    fix(2)

--- components/main/AppTransferDialog.tsx
+++ components/main/AppTransferDialog.tsx
@@ -12,7 +12,7 @@ import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Armchair, RefreshCcw, Sparkle, Sparkles, ArrowDownUp } from "lucide-react";
 
-export default function AppTransferDialog() {
+export default function AppTransferDialog({player, currentFixtures}: any) {
   return (
     <Dialog>
       <DialogTrigger asChild>
