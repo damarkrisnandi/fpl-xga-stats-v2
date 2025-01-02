dc6d0beff687479f72b518498998c5ccd02ba4c7
damar <damar@gitlab.lab>
2025-01-02 11:19:55 +0700

    fix error build

--- components/react-query/MainProvider.tsx
+++ components/react-query/MainProvider.tsx
@@ -1,16 +1,22 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import React from 'react';
 
 // Create a new instance of QueryClient
 const queryClient = new QueryClient();
 
 // Define the HOC
 const withQueryClientProvider = (WrappedComponent: React.ComponentType) => {
-    return (props: any) => (
+    const WithQueryClientProvider = (props: any) => (
         <QueryClientProvider client={queryClient}>
             {/* You can add any additional logic or components here */}
-            <WrappedComponent {...props}/>
+            <WrappedComponent {...props} />
         </QueryClientProvider>
     );
+
+    // Set the display name for the HOC
+    WithQueryClientProvider.displayName = `WithQueryClientProvider(${WrappedComponent.displayName || WrappedComponent.name || 'Component'})`;
+
+    return WithQueryClientProvider;
 };
 
 export default withQueryClientProvider;
